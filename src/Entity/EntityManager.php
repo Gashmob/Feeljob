290493d7b4a505b7dfa05a0b5732f3fdf54f915f
Gashmob <kevin@ktraini.com>
2021-01-09 14:07:37 +0100

    Add email used + send verification email

--- src/Entity/EntityManager.php
+++ src/Entity/EntityManager.php
@@ -4,15 +4,28 @@
 namespace App\Entity;
 
 
+use App\database\PreparedQuery;
+
 /**
  * Class EntityManager
  * @package App\Entity
  */
 abstract class EntityManager
 {
+    /**
+     * @param string $mail
+     * @return bool
+     */
+    public static function isMailUsed(string $mail): bool
+    {
+        return count((new PreparedQuery('MATCH (u {mail:$mail}) RETURN u'))
+            ->setString('mail', $mail)
+            ->getResult()) > 0;
+    }
+
     /* -- Example --
 
-    public function getUserFromUsername(string $username)
+    public static function getUserFromUsername(string $username)
     {
         return (new PreparedQuery('MATCH (u:User {name:$name}) RETURN u'))
             ->setString('name', $username)
