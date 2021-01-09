519dacc09d9ba07648ac12b0f0031414bb1d5391
Luca <luca.ciampi@etu.univ-smb.fr>
2021-01-09 14:12:52 +0100

    jquery, semantic ui

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-console.log('ok');
+$(document)
+    .ready(function() {
+        $('.ui.menu .ui.dropdown').dropdown({
+            on: 'hover'
+        });
+        $('.ui.menu a.item')
+            .on('click', function() {
+                $(this)
+                    .addClass('active')
+                    .siblings()
+                    .removeClass('active')
+                ;
+            })
+        ;
+    })
+;
