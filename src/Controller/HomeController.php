4fc9cdba2c4551241f6f95f02c278ef23024dede
Gashmob <kevin@ktraini.com>
2021-01-09 10:34:59 +0100

    Add HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(): Response
+    {
+        return $this->render('home/homepage.html.twig');
+    }
+}
