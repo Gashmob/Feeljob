e1c4a21bbdd1eb31fd0cc410548aa49680e396c8
florian <deschenf@gmail.com>
2021-03-17 17:53:11 +0100

    implementation of AnnonceManager

--- src/database/manager/AnnonceManager.php
+++ src/database/manager/AnnonceManager.php
@@ -3,39 +3,63 @@
 
 namespace App\database\manager;
 
+use App\database\PreparedQuery;
+use App\database\Query;
 
 class AnnonceManager extends Manager
 {
 
-    /**
-     * @inheritDoc
-     */
-    public function find(int $id): string
-    {
-        // TODO: Implement find() method.
-    }
+	/**
+	 * @inheritDoc
+	 */
+	public function find(int $id): ?string
+	{
+		$result = (new PreparedQuery('MATCH (a:Annonce) WHERE id(a)=$id RETURN id(a) as id'))
+			->setInteger('id', $id)
+			->run()
+			->getOneOrNullResult();
+		return $result == null ? null : $result['id'];
+	}
 
-    /**
-     * @inheritDoc
-     */
-    public function findOneBy(array $filters): string
-    {
-        // TODO: Implement findOneBy() method.
-    }
+	/**
+	 * @inheritDoc
+	 */
+	public function findOneBy(array $filters): string
+	{
+		$query = 'MATCH (a:Annonce) WHERE ';
+        foreach ($filters as $filter)
+            $query .= $filter . '=' . $filters[$filter];
+        $query .= ' RETURN id(a) as id';
 
-    /**
-     * @inheritDoc
-     */
-    public function findAll(): array
-    {
-        // TODO: Implement findAll() method.
-    }
+        $result = (new Query($query))
+            ->run()
+            ->getOneOrNullResult();
 
-    /**
-     * @inheritDoc
-     */
-    public function findBy(array $filters): array
-    {
-        // TODO: Implement findBy() method.
-    }
+        return $result == null ? null : $result['id'];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function findAll(): array
+	{
+		return (new Query('MATCH (a:Annonce) RETURN id(a) as id'))
+			->run()
+			->getResult();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function findBy(array $filters): array
+	{
+		$query = 'MATCH (a:Annonce) WHERE ';
+		foreach ($filters as $filter)
+			$query .= $filter . '=' . $filters[$filter];
+		$query .= ' RETURN id(a) as id';
+
+		return (new Query($query))
+			->run()
+			->getResult();
+	}
 }
