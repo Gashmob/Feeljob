c7dd0d2ad7cfabf1b77def5e7819131c9245606f
florian <florian.deschenaux@etu.univ-smb.fr>
2021-03-25 10:50:30 +0100

    ajout fonctions AnnonceManager

--- src/database/manager/AnnonceManager.php
+++ src/database/manager/AnnonceManager.php
@@ -5,6 +5,7 @@ namespace App\database\manager;
 
 use App\database\PreparedQuery;
 use App\database\Query;
+use App\database\Annonce;
 
 class AnnonceManager extends Manager
 {
@@ -62,4 +63,52 @@ class AnnonceManager extends Manager
 			->run()
 			->getResult();
 	}
+
+
+	/**
+	 * @param EntityManagerInterface $em
+	 * @param Annonce $annonce
+	 * @param int $idParticulier
+	 * @param string $secteurActivite
+	 * @return int|null
+	 */
+	public function create(EntityManagerInterface $em,Annonce $annonce,string $idParticulier,string $secteurActivite) : ?int
+	{
+		$result = (new PreparedQuery('MATCH (p:' . EntityManager::PARTICULIER .'),(s:' . EntityManager::SECTEUR_ACTIVITE . '{nom:$secteur}) WHERE id(p)=$idParticulier CREATE (p)-[:' . EntityManager::PUBLIE . ']->(a: ' . EntityManager::ANNONCE .')-[:' . EntityManager::EST_DANS .']->(s) RETURN id(a) AS id'))
+		->setString('secteur',$secteurActivite)
+		->setInteger('idParticulier',$idParticulier)
+		->run()
+		->getOneOrNullResult();
+
+		if($result !=null) {
+			$idAnnonce = $result['id'];
+			$annonce->setIdentity($idAnnonce);
+			$em->persist($annonce);
+			$em->flush();
+			return $idAnnonce
+		}
+		return null;
+	}
+
+
+	/**
+	 * @param EntityManagerInterface $em
+	 * @param Annonce $annonce
+	 */
+	public function remove(EntityManagerInterface $em,Annonce $annonce){
+		(new PreparedQuery('MATCH (a:' . EntityManager::ANNONCE . ')-[r]-() WHERE id(a)=$id DELETE r,a'))
+		->setInteger('id',$annonce->getIdentity())
+		->run();
+
+		$em->remove($annonce);
+		$em->flush();
+	}
+
+	/**
+     * @param EntityManagerInterface $em
+     */
+    public function update(EntityManagerInterface $em)
+    {
+        $em->flush();
+    }
 }
