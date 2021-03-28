38fadbe566a1af946f578c6e43183531a45f3079
Gashmob <kevin@ktraini.com>
2021-03-28 12:13:59 +0200

    Add candidate & uncandidate & propose & removeProposition

--- src/Controller/ParticulierController.php
+++ src/Controller/ParticulierController.php
@@ -294,6 +294,108 @@ class ParticulierController extends AbstractController
     // AJAX
 
     /**
+     * @Route("/candidate/{id}", requirements={"id": true}, methods={"POST"})
+     * @param $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function candidate($id, Request $request): JsonResponse
+    {
+        if (!($this->session->get('user'))) {
+            return $this->json(['result' => false]);
+        }
+
+        if (EntityManager::getRepository(EntityManager::UTILS)->getUserTypeFromId($this->session->get('user')) != EntityManager::AUTO_ENTREPRENEUR) {
+            return $this->json(['result' => false]);
+        }
+
+        if ($request->isMethod('POST')) {
+            return $this->json([
+                'result' => EntityManager::getRepository(EntityManager::ANNONCE)->candidate($id, $this->session->get('user'))
+            ]);
+        }
+
+        return $this->json(['result' => false]);
+    }
+
+    /**
+     * @Route("/uncandidate/{id}", requirements={"id": true}, methods={"POST"})
+     * @param $id
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function uncandidate($id, Request $request): JsonResponse
+    {
+        if (!($this->session->get('user'))) {
+            return $this->json(['result' => false]);
+        }
+
+        if (EntityManager::getRepository(EntityManager::UTILS)->getUserTypeFromId($this->session->get('user')) != EntityManager::AUTO_ENTREPRENEUR) {
+            return $this->json(['result' => false]);
+        }
+
+        if ($request->isMethod('POST')) {
+            return $this->json([
+                'result' => EntityManager::getRepository(EntityManager::ANNONCE)->uncandidate($id, $this->session->get('user'))
+            ]);
+        }
+
+        return $this->json(['result' => false]);
+    }
+
+    /**
+     * @Route("/propose/{idAnn}/{idAuto}", requirements={"idAnn": true, "idAuto": true}, methods={"POST"})
+     * @param $idAnn
+     * @param $idAuto
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function propose($idAnn, $idAuto, Request $request): JsonResponse
+    {
+        if (!($this->session->get('user'))) {
+            return $this->json(['result' => false]);
+        }
+
+        if (EntityManager::getRepository(EntityManager::UTILS)->getUserTypeFromId($this->session->get('user')) != EntityManager::PARTICULIER) {
+            return $this->json(['result' => false]);
+        }
+
+        if ($request->isMethod('POST')) {
+            return $this->json([
+                'result' => EntityManager::getRepository(EntityManager::ANNONCE)->propose($idAnn, $idAuto)
+            ]);
+        }
+
+        return $this->json(['result' => false]);
+    }
+
+    /**
+     * @Route("/remove/proposition/{idAnn}/{idAuto}", requirements={"idAnn": true, "idAuto": true}, methods={"POST"})
+     * @param $idAnn
+     * @param $idAuto
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function removeProposition($idAnn, $idAuto, Request $request): JsonResponse
+    {
+        if (!($this->session->get('user'))) {
+            return $this->json(['result' => false]);
+        }
+
+        if (EntityManager::getRepository(EntityManager::UTILS)->getUserTypeFromId($this->session->get('user')) != EntityManager::PARTICULIER) {
+            return $this->json(['result' => false]);
+        }
+
+        if ($request->isMethod('POST')) {
+            return $this->json([
+                'result' => EntityManager::getRepository(EntityManager::ANNONCE)->removeProposition($idAnn, $idAuto)
+            ]);
+        }
+
+        return $this->json(['result' => false]);
+    }
+
+    /**
      * @Route("/accept/proposition/{id}", requirements={"id": true}, methods={"POST"})
      * @param $id
      * @param Request $request
