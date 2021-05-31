3ff576f82c0becd78d2415e1b31dbf656fc397f3
Luca <luca.ciampi@etu.univ-smb.fr>
2021-05-31 22:00:25 +0200

    CSS autoprefix + JS Compatibility

--- public/js/visiteur.js
+++ public/js/visiteur.js
@@ -3,14 +3,14 @@
  */
 
 // Tronque les descriptions
-const MAX_DESCRIPTION_LENGTH = 300;
+var MAX_DESCRIPTION_LENGTH = 300;
 
 function truncate(str, n, useWordBoundary
 ) {
     if (str.length <= n) {
         return str;
     }
-    const subString = str.substr(0, n - 1); // the original check
+    var subString = str.substr(0, n - 1); // the original check
     return (useWordBoundary
         ? subString.substr(0, subString.lastIndexOf(" "))
         : subString) + " &hellip;";
